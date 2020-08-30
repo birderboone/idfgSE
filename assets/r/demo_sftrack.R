e65b41deec03ed131db134acbab1e6fc0029fce0
birderboone <mttboone@gmail.com>
2020-08-30 16:29:16 -0600

    Sunday updates

---
+++ assets/r/demo_sftrack.R
@@ -0,0 +1,19 @@
+# Load in libraries
+library(sftrack)
+library(lubridate)
+
+# initial setup
+data(raccoon)
+projection <- '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+raccoon$timestamp <- ymd_hms(raccoon$timestamp)
+raccoon$month <- month(raccoon$timestamp, label = TRUE)
+
+# create object
+my_sftrack <- as_sftrack(data = raccoon, coords = c('longitude','latitude'),
+                         crs = projection, burst = c(id = 'animal_id', month = 'month'), 
+                         active_burst = 'id',time = 'timestamp')
+
+
+View(my_sftrack)
+
+head(my_sftrack)
